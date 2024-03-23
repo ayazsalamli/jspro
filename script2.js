24bb468a52e1b1b2d67312d925778bff4ddaec43
erlams <elmaddin@gmail.com>
2024-03-23 17:40:06 +0100

    sdjnm,

---
+++ script2.js
@@ -0,0 +1,37 @@
+const movieDB = {
+    movies: [
+        "Logan",
+        "Titanic",
+        "Bəyin oğurlanması",
+        "Xaç atası",
+        "Avatar"
+    ]
+}
+
+const reklam = document.querySelectorAll('.promo_adv img');
+const array = document.querySelector('.promo_genre');
+const yeniKino = document.querySelector('.promo_interactive-list');
+const promo_interactive = document.querySelector('.promo_interactive-item')
+const delete_Icon = document.querySelector('.delete custom-cursor-on-hover')
+const promo_interactiveL = document.querySelectorAll('.promo_interactive-list')
+
+reklam.forEach(sekil => {
+    sekil.remove();
+});
+
+array.innerHTML = 'Drama';
+
+yeniKino.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((kino, i) => {
+    yeniKino.innerHTML += `
+            <li class="promo_interactive-item">${i + 1}.${kino}
+                <div class="delete"></div>
+            </li>
+
+   `
+});
+
+
